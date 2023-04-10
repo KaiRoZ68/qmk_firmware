2c8f295726e2dc49195f0eb51948ef3815dff967
Kai von Rotsmann <kai.rotsmann@mailbox.org>
2023-04-10 16:48:06 +0200

    Letzte Schönheitskorrektur vor dem Umbau der Tab Dance Funktionen.

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/KaiRoZ68/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/KaiRoZ68/keymap.c
@@ -324,6 +324,4 @@ tap_dance_action_t tap_dance_actions[] = {
         [DANCE_2] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_2, dance_2_finished, dance_2_reset),
         [DANCE_3] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_3, dance_3_finished, dance_3_reset),
         [DANCE_4] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_4, dance_4_finished, dance_4_reset),
-
-
 };
