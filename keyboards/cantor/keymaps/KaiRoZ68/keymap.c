01a46efbfc24474bd5f022d3abca6e76d2924c2c
Kai von Rotsmann <kai.rotsmann@mailbox.org>
2023-04-15 19:13:37 +0200

    Keymap für Kyria vor meiner Umarbeitung

--- keyboards/cantor/keymaps/KaiRoZ68/keymap.c
+++ keyboards/cantor/keymaps/KaiRoZ68/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_split_3x6_3(
     QK_GESC,       KC_Q,    KC_W,    KC_E,   KC_R,      KC_T,                                      KC_Y,           KC_U,            KC_I,           KC_O,           KC_P,    KC_BSPC,
-    LSFT(KC_TAB),  KC_A,    KC_S ,   KC_D,   KC_F,      KC_G,                                      KC_H,           KC_J,            KC_K,           KC_L,           KC_SCLN, RSFT(KC_ENTER),
+    KC_TAB,  KC_A,    KC_S ,   KC_D,   KC_F,      KC_G,                                      KC_H,           KC_J,            KC_K,           KC_L,           KC_SCLN, KC_ENTER,
     OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,   KC_V,      KC_B,                                      KC_N,           KC_M,            KC_COMM,        KC_DOT,         KC_SLSH, OSM(MOD_RSFT),
                                              KC_LCTL,   LOWER,    LALT_T(KC_INS),       KC_SPACE,  RAISE,          RALT_T(KC_DEL)
 ),
@@ -69,31 +69,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Raise */
 
 [_RAISE] = LAYOUT_split_3x6_3(
-    KC_GRV,  TD(DANCE_1),   TD(DANCE_2),    TD(DANCE_3),    TD(DANCE_4),                                                            KC_5,       KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     _______,
-    _______, KC_F11,        KC_F12,         KC_PGUP,        KC_PGDN,                                                                KC_HOME,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_END,   _______,
-    _______, KC_F1,         KC_F2,          KC_F3,          KC_F4,                                                                  KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10 ,  _______,
-                                            _______,        _______,        _______,                                    _______,    _______,   _______
+    KC_GRV,  TD(DANCE_1),   TD(DANCE_2),    TD(DANCE_3),    TD(DANCE_4),    KC_5,                                       KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     _______,
+    _______, KC_F11,        KC_F12,         KC_PGUP,        KC_PGDN,        KC_HOME,                                    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_END,   _______,
+    _______, KC_F1,         KC_F2,          KC_F3,          KC_F4,          KC_F5,                                      KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10 ,  _______,
+                                                            _______,        _______,    _______,            _______,    _______,   _______
 ),
 
 /* Adjust (Lower + Raise) */
 
 [_ADJUST] = LAYOUT_split_3x6_3(
-    _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD,  _______ ,
+    _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, CG_TOGG, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD,  _______ ,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______
-  ),
+  )
 
 
-/* Layer Numpad */
+/* Layer Numpad
 
 [_NUMPAD] = LAYOUT_split_3x6_3(
-    _______, KC_HOME , KC_UP,   KC_END,   _______, _______, _______,  KC_KP_7,  KC_KP_8,  KC_KP_9, KC_PSLS, _______,
-    _______, KC_LEFT, KC_DOWN,  KC_RIGHT, _______, _______, _______,  KC_KP_4,  KC_KP_5,  KC_KP_6, KC_PAST, KC_PENT,
-    _______, _______, _______,  _______,  _______, _______, _______,  KC_KP_1,  KC_KP_2,  KC_KP_3, KC_PMNS, _______,
-    NUMP   ,           KC_KP_0,  KC_PDOT,  KC_DOT , KC_PPLS, KC_NUM
+    _______, KC_HOME , KC_UP,   KC_END,   _______, _______, _______,    KC_KP_7,  KC_KP_8,  KC_KP_9, KC_PSLS, _______,
+    _______, KC_LEFT, KC_DOWN,  KC_RIGHT, _______, _______, _______,    KC_KP_4,  KC_KP_5,  KC_KP_6, KC_PAST, KC_PENT,
+    _______, _______, _______,  _______,  _______, _______, _______,    KC_KP_1,  KC_KP_2,  KC_KP_3, KC_PMNS, _______,
+    NUMP   ,                                                KC_PPLS,    KC_KP_0,  KC_PDOT,  KC_DOT , , KC_NUM
 )
-
+*/
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
