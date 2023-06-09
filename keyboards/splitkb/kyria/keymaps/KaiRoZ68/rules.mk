636c8ff10281b72b8b863b11580b786eee17adc1
Kai von Rotsmann <kai.rotsmann@mailbox.org>
2023-06-09 19:38:05 +0200

    rules.mk reduziert um einige Treiber reduziert.

--- keyboards/splitkb/kyria/keymaps/KaiRoZ68/rules.mk
+++ keyboards/splitkb/kyria/keymaps/KaiRoZ68/rules.mk
@@ -1,9 +1,11 @@
-OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+#OLED_DRIVER = SSD1306      # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 MOUSEKEY_ENABLE = yes
-RGB_MATRIX_ENABLE = yes    # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+#RGB_MATRIX_ENABLE = yes    # Disable keyboard RGB matrix, as it is enabled by default on rev3
+#RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 BACKLIGHT_DRIVER = software
 CAPS_WORD_ENABLE = yes
 TAP_DANCE_ENABLE = yes      # Tap Dance
-WS2812_DRIVER = vendor
+#WS2812_DRIVER = vendor
+OLED_DRIVER_ENABLE = yes
+WPM_ENABLE = yes
