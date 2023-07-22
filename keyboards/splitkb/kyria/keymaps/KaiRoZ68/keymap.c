fed479e7ea1c3512a57a38e458ceaf540d0c63c9
Kai von Rotsmann <kai.rotsmann@mailbox.org>
2023-07-22 11:24:05 +0200

    funktionierender Firmwarestand

--- keyboards/splitkb/kyria/keymaps/KaiRoZ68/keymap.c
+++ keyboards/splitkb/kyria/keymaps/KaiRoZ68/keymap.c
@@ -37,7 +37,7 @@ enum tap_dance_codes {
 };
 
 #define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define RAISE LT(_RAISE,KC_BSPC)
 #define NUMP TG(_NUMPAD)
 #define ADJU MO(_ADJUST)
 #define ARMA TG(_ARMA)
@@ -78,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
     QK_GESC,       KC_Q,    KC_W,    KC_E,      KC_R,          KC_T,                                                                    KC_Y,   KC_U,           KC_I,       	KC_O,     KC_P,    TD(DANCE_5),
-    KC_TAB,        KC_A,    KC_S ,   KC_D,      KC_F,          KC_G,                                                                    KC_H,   KC_J,           KC_K,       	KC_L,     KC_SCLN, KC_ENTER,
+    KC_TAB,        KC_A,    LALT_T(KC_S), LCTL_T(KC_D),  LGUI_T(KC_F),  KC_G,                                                           KC_H,   RGUI_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L),     KC_SCLN, KC_ENTER,
     KC_LSFT,       LSFT_T(KC_Z),    KC_X,    KC_C,      KC_V,          KC_B,    CW_TOGG,   ARMA,                NUMP,  		Mouse,    			KC_N,   KC_M,           KC_COMM,    	KC_DOT,   RSFT_T(KC_SLSH), KC_RSFT,
-                                     KC_MUTE,   OSM(MOD_LCTL), LOWER,   LGUI_T(KC_TAB) ,  LALT_T(KC_ENTER),      	RALT_T(KC_BSPC),    KC_SPC, RAISE,  		OSM(MOD_RCTL),  XXXXXXX
+                                     KC_MUTE,   OSM(MOD_LCTL), LOWER,   LGUI_T(KC_ENTER) ,  LALT_T(KC_ENTER),      	RALT_T(KC_SPC),    KC_SPC, RAISE,  		OSM(MOD_RCTL),  XXXXXXX
 ),
 
 /* Lower */
